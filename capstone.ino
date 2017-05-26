bc150f82fbeb4f4882ec7f34871adaa833557b09
Ben Yang <thisisbenbtw@gmail.com>
2017-05-26 13:08:45 -0700

    PID with updated sensor values

--- capstone.ino
+++ capstone.ino
@@ -2,7 +2,6 @@
 #define LEFT 17
 #define RIGHT 19
 
-
 #define PWM1 10
 #define PWM2 11
 #define IN1 5
@@ -10,7 +9,7 @@
 #define IN3 7
 #define IN4 8
 
-int leftS = 0, rightS = 0;
+int leftS = 940, rightS = 940;
 
 void setup() {
   // put your setup code here, to run once:
@@ -26,7 +25,7 @@ void setup() {
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
-
+/*
     int leftSum = 0, rightSum = 0;
   for (int i = 0; i < 20; i++) {
     leftSum += analogRead(LEFT);
@@ -35,26 +34,38 @@ void setup() {
   leftS = leftSum / 20;
   rightS = rightSum / 20;
   
+  
+  Serial.print(leftS);
+  Serial.print(' ');
+  Serial.println(rightS);
+  */
   Serial.begin(9600);
 }
 
 int KI=1,KD=1,KP=1;
 int dt=10;
-int speed1 = 80;
+int speed1 = 95;
 int speed2 = 95;
+int pError=0;
+int integral=0;
 
 void loop() {
   // put your main code here, to run repeatedly:
   int lerr = -analogRead(LEFT) + leftS;
   int rerr = analogRead(RIGHT) - rightS;
   int err = (lerr+rerr)/2;
-  int correction = KP*err;
-//  Serial.println(correction);
+  
+  int derivative = (pError-err)/dt;
+  pError = err;
+  integral += err*dt;
+  
+  Serial.println(analogRead(FRONT));
+  int correction = KP*err + KD*derivative;
 
-
+  
 
   int s1 = speed1-correction;
-  int s2 = speed2+correction*speed1/speed2;
+  int s2 = speed2+correction*3/4;
 
   if(s1<0)
     s1 = 0;
@@ -65,9 +76,7 @@ void loop() {
   else if(s2>150)
     s2 = 150;
 
-  Serial.print(s1);
-  Serial.print(' ');
-  Serial.println(s2);
+  //Serial.println(err);
     //turning? make it go forward
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
